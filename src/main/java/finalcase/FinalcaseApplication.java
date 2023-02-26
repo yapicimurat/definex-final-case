e5a5e59e35d18b50d08e09a9b7d8e4e86892df71
yapicimurat <muratyapiicii@gmail.com>
2023-02-26 11:14:04 +0300

    Create Spring boot project

---
+++ src/main/java/finalcase/FinalcaseApplication.java
@@ -0,0 +1,13 @@
+package finalcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalcaseApplication.class, args);
+	}
+
+}
